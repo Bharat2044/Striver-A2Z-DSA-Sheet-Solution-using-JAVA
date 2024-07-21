5d0c8d53fbb9e84a8698fc78f8d721b59204069c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-07-22 04:27:56 +0530

    Pattern Program 8 solution code added

---
+++ Step 1 : Learn the basics/Lec 2: Build-up Logical Thinking/08_PatternProgram.java
@@ -0,0 +1,20 @@
+// Question Link: https://www.naukri.com/code360/problems/reverse-star-triangle_6573685?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+
+public class Solution {
+    public static void nStarTriangle(int n) {
+        
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= i - 1; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2*n - 2*i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
