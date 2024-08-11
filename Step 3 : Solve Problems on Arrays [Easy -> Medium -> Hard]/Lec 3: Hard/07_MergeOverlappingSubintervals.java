121330e9d05b006db16ba26eedf76fe2439a3b70
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-08-11 18:02:47 +0530

    Merge Overlapping Subintervals solution code added

---
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Lec 3: Hard/07_MergeOverlappingSubintervals.java
@@ -0,0 +1,27 @@
+// Question Link: https://leetcode.com/problems/merge-intervals/description/
+
+
+class Solution {
+    // Time Complexity = O(n * logn), Space Complexity = O(n)
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
+            }
+        }
+
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);         
+    }
+}
