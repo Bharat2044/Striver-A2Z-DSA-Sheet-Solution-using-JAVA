30a34fafab592d0beafbaaf86578e0a1d3031f96
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-08-19 13:55:13 +0000

    Functions (Pass by Reference and Value) solution code added

---
+++ Step 01 : Learn the basics/Lec 1: Things to Know in Java or any language/08_FunctionsPassByReferenceAndValue.java
@@ -0,0 +1,11 @@
+// Question Link: https://www.geeksforgeeks.org/problems/pass-by-reference-and-value/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pass-by-reference-and-value
+
+
+class Solution {
+    
+    // Time Complexity = O(1), Space Complexity = O(1)
+    static int[] passedBy(int a, int b) {
+        // code here
+        return new int[]{a + 1, b + 2};
+    }
+}
