d3b9c577415caa7c253d357e8ffd0b9e6a3ce15a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-08-19 14:21:23 +0000

    Maximum Product Subarray solution code added

---
+++ Step 03 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Lec 3: Hard/12_MaximumProductSubarray.java
@@ -0,0 +1,26 @@
+// Question Link: https://leetcode.com/problems/maximum-product-subarray/description/
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int max = nums[0], min = nums[0], ans = nums[0];
+        int n = nums.length;
+        
+        for (int i = 1; i < n; i++) {
+        
+            if (nums[i] < 0){
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+
+            ans = Math.max(ans, max);
+        }
+        
+        return ans;
+    }
+}
