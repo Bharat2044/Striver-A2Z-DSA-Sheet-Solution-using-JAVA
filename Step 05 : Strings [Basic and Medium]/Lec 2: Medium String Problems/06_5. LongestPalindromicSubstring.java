f51d2e303d9189279cafa61fb64bb339843df04f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-09-14 18:37:10 +0530

    Longest Palindromic Substring[Do it without DP] solution code added

---
+++ Step 05 : Strings [Basic and Medium]/Lec 2: Medium String Problems/06_5. LongestPalindromicSubstring.java
@@ -0,0 +1,37 @@
+// Question Link: https://leetcode.com/problems/longest-palindromic-substring/description/
+
+
+public class Solution {
+
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
+    }
+
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            String odd = expandFromCenter(s, i, i);
+            String even = expandFromCenter(s, i, i + 1);
+
+            if (odd.length() > maxStr.length()) {
+                maxStr = odd;
+            }
+            
+            if (even.length() > maxStr.length()) {
+                maxStr = even;
+            }
+        }
+
+        return maxStr;
+    }
+}
